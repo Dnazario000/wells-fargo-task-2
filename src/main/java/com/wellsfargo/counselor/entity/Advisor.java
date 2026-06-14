aa2dd33bda1b0a0d2cf33e5bc1ec6badef94ac15
Dnazario000 <166957802+Dnazario000@users.noreply.github.com>
2026-06-14 09:33:19 -0400

    Add clients relationship to Advisor entity

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.util.List;
 
 @Entity
 public class Advisor {
@@ -27,6 +28,9 @@ public class Advisor {
 
     @Column(nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "advisor")
+    private List<Client> clients;
 
     protected Advisor() {
 
