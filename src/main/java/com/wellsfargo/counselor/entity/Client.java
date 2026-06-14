b326e16621b502ce8bf4489ee25267d0977ac949
Dnazario000 <166957802+Dnazario000@users.noreply.github.com>
2026-06-14 09:47:59 -0400

    Refactor Client class to remove portfolio association
    Updated Client class to remove portfolio field and adjust constructor.

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,46 +1,52 @@
 package com.wellsfargo.counselor.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Client {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue()
     private Long clientId;
 
     @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
     private Advisor advisor;
 
-    @Column
+    @Column(nullable = false)
     private String firstName;
 
-    @Column
+    @Column(nullable = false)
     private String lastName;
 
-    @Column
+    @Column(nullable = false)
     private String address;
 
-    @Column
+    @Column(nullable = false)
     private String phone;
 
-    @Column
+    @Column(nullable = false)
     private String email;
 
     @OneToOne(mappedBy = "client")
     private Portfolio portfolio;
 
-    public Client() {
+    protected Client() {
     }
 
-    public Client(Advisor advisor, String firstName, String lastName, String address, String phone, String email, Portfolio portfolio) {
+    public Client(Advisor advisor, String firstName, String lastName, String address, String phone, String email) {
         this.advisor = advisor;
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.phone = phone;
         this.email = email;
-        this.portfolio = portfolio;
     }
 
     public Long getClientId() {
@@ -53,6 +59,10 @@ public class Client {
 
     public void setAdvisor(Advisor advisor) {
         this.advisor = advisor;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
     }
 
     public String getFirstName() {
@@ -93,13 +103,5 @@ public class Client {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-    
-    public Portfolio getPortfolio() {
-        return portfolio;
-    }
-    
-    public void setPortfolio(Portfolio portfolio) {
-        this.portfolio = portfolio;
     }
 }
