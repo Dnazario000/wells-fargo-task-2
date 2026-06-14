2fa9eb931d105ab00d84609b2c23bbbf8995bdc6
Dnazario000 <166957802+Dnazario000@users.noreply.github.com>
2026-06-14 09:45:24 -0400

    Update advisorId type to Long in Advisor entity
    Changed advisorId type from long to Long for nullability.

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -5,14 +5,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.util.List;
+
 
 @Entity
 public class Advisor {
 
     @Id
     @GeneratedValue()
-    private long advisorId;
+    private Long advisorId;
 
     @Column(nullable = false)
     private String firstName;
